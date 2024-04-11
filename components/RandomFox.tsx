1709a08293883a0b52da63360b5e160a1e49d39e
Ing. Marvin Menchu <marvin.menchu@icloud.com>
2024-04-11 12:10:03 -0600

    LAZY LOADING CON OBSERVADOR

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,21 +1,58 @@
-import { FC, FunctionComponent } from 'react';
+import { useRef, useEffect, useState } from 'react'
 
-export const RandonFox2 = () => {
-    return <img />
-}
-
-//generar a random function  1 a 123
 
 type Props = {image: string, alt: string}
 
 export const RandonFox = ({image, alt}: Props): JSX.Element => { // recomendado para usar
-    return <img width={320} height="auto" className="rounded" src={image} />
+    const node = useRef<HTMLImageElement>(null)
+    const [src, setSrc] = useState(
+        '"data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4='
+    )
+
+    useEffect(() => {
+        // nuevo observador
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                // onInteraction -> console.log
+                if (entry.isIntersecting) {
+                    setSrc(image)
+                }
+            })
+        })
+
+    //observe node
+    if (node.current){
+        observer.observe(node.current)
+    }
+
+    //desconectar
+    return () => observer.disconnect()
+
+    }, [image])
+        
+    
+
+    
+
+    
+
+
+    return (<img ref={node} width={320} height="auto" className="rounded bg-gray-300" src={src} />)
 }
 
-export const RandonFox3: FunctionComponent = () => {
-    return <img />
-}
 
-export const RandonFox4: FC = () => {
-    return <img />
-}
+
+
+
+
+// export const RandonFox2 = () => {
+//     return <img />
+// }
+
+// export const RandonFox3: FunctionComponent = () => {
+//     return <img />
+// }
+
+// export const RandonFox4: FC = () => {
+//     return <img />
+// }
